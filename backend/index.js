8adec13248715fdeee31a96620e827f556562d73
DRAGON <anantvijay3@gmail.com>
2022-10-11 03:16:16 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import express from "express"
 const app = express()
 app.use(express.json())
 
+let users = [{name: "Tom", gender: "Male", bio: "Loves to paint"}, {name: "Jerry", gender: "Male", bio: "Loves sleeping"}]
+
 app.get("/", (req, res) => {
     res.json("Hello World")
 })
