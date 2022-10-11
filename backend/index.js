b2b6876ebce318035c64c241b753c7ad42703ccc
romantic-sid <114246776+romantic-sid@users.noreply.github.com>
2022-10-11 17:13:26 +0530

    Issue 5 (#17)
    * Add the get route
    
    * Add the mock data to user array
    
    * Created the new route
    
    * Created the New  Route
    
    * create the new route
    
    * create the new route
    
    * resolve merge conflict

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,21 @@ let users = [{id:1 ,name: "Tom", gender: "Male", bio: "Loves to paint"}, {id:2,n
 app.get("/", (req, res) => {
     res.json("Hello World")
 })
+
 app.get('/users/',(req,res)=>{
     res.json(users)
+})
+
+app.get('/users/:id',(req,res)=>{
+    const {id}=req.params
+    const getUser=users.find((user)=>user.id===Number(id))
+    if(!getUser){
+        return res.status(404).send('User not found')
+    }
+    else{
+        return res.json(getUser)
+    }
+    
 })
 
 const PORT = 8080
@@ -17,3 +30,4 @@ const PORT = 8080
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`)
 })
+
