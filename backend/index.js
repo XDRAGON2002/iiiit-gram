68ef8fdf6e593879409cf0c2707dce3ddf466524
Hariom Sharma <89256126+Hariom-Starlink90@users.noreply.github.com>
2022-10-13 16:13:37 +0530

    user/update/id route added

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import userRoutes from "./routes/user.js";
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({extended : false}));
 
 app.get("/", (req, res) => {
   res.json("Hello World");
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -30,6 +30,28 @@ router.delete("/delete/:id",(req,res)=>{
     users=[...users.slice(0,userIndex),...users.slice(userIndex+1)];
     return res.json(deleteduser);
   }
-})
+});
+
+router.put("/update/:id",(req,res)=>{
+  const { id } = req.params;
+  const userIndex = users.findIndex((user)=>user.id===Number(id));
+  const {name,gen,bio}=req.body;
+  if (userIndex===-1) {
+    return res.status(404).send("User not found");
+  }
+  else if(gen==='Male'||gen==='Female'||!gen) 
+  {
+    users[userIndex].name=(name)?name:users[userIndex].name;
+    users[userIndex].gender=(gen)?gen:users[userIndex].gender;
+    users[userIndex].bio=(bio)?bio:users[userIndex].bio;
+    return res.json(users[userIndex]);
+  }
+  else {
+    // console.log(gen)
+    return res.status(400).send("Incorrect entry in gender field");
+  }
+});
+
+
 
 export default router;
