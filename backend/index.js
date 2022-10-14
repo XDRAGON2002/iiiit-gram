40d9dd00e91c1bad40fe3dbc9e8f49778f323bf6
Siddharth Jaiswal <sid200203@gmail.com>
2022-10-14 10:00:29 +0530

    Integrate the MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,18 @@
+import  mongoose from 'mongoose'
+import * as dotenv from 'dotenv'
+dotenv.config()
 import express from "express"
+ 
+
+const connectionParams = {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}
+
+mongoose.connect(process.env.DATABASE_URL,connectionParams)
+.then(()=>{
+    console.log('Connection Created')
+}).catch((err)=>console.log('no connection'))
 
 const app = express()
 app.use(express.json())
