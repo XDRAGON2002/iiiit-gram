a14ff64c78371388a4444d28f387c20728f0c1dc
romantic-sid <114246776+romantic-sid@users.noreply.github.com>
2022-10-14 10:55:00 +0530

    Issue 9 (#31)
    * Add the get route
    
    * Add the mock data to user array
    
    * Created the new route
    
    * Created the New  Route
    
    * create the new route
    
    * create the new route
    
    * resolve merge conflict
    
    * create the poost route
    
    * create the post route
    
    * Update the issue-6
    
    * update the issue
    
    * remove the tabs
    
    * Integrate the MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,18 @@
+import  mongoose from 'mongoose'
+import * as dotenv from 'dotenv'
+dotenv.config()
 import express from "express";
 import userRoutes from "./routes/user.js";
+const connectionParams = {
+  useNewUrlParser:true,
+  useUnifiedTopology:true
+}
+
+mongoose.connect(process.env.DATABASE_URL,connectionParams)
+.then(()=>{
+  console.log('Connection Created')
+}).catch((err)=>console.log('no connection'))
+
 
 const app = express();
 app.use(express.json());
